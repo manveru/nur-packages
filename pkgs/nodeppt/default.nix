321ac52b3c4b7dd752b16893fb364ab169c21a64
Jörg Thalheim <joerg@thalheim.io>
2018-07-16 15:26:34 -0400

    nodeppt: use fetchFromGitHub instead of fetchGit

--- pkgs/nodeppt/default.nix
+++ pkgs/nodeppt/default.nix
@@ -1,9 +1,11 @@
-{ yarn2nix }:
+{ yarn2nix, fetchFromGitHub }:
 yarn2nix.mkYarnPackage {
   name = "nodeppt";
-  src = fetchGit {
-    url = "https://github.com/ksky521/nodeppt";
+  src = fetchFromGitHub {
+    owner = "ksky521";
+    repo = "nodeppt";
     rev = "5c6475554297f5cd2f0fb7521a15ba927f90f9bb";
+    sha256 = "0ydwgy8w9d785bcc22a298hzwf4qa9sm9d0a40v037cmr2qqsh1v";
   };
   extraBuildInputs = [ ];
   packageJson = ./package.json;
